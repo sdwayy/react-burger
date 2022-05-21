cbf604572784e17a7fc0c057d473c0e6f7ff065e
Daniil Pugachev <pugacdan@gmail.com>
2022-05-21 13:28:04 +0300

    Replace BrowserRouter to HashRouter

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import moment from 'moment';
 import 'moment/locale/ru';
 
